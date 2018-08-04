ac89f524be47adf1551abecec57827dc97f2a7d8
minhash <minorhash@gmail.com>
2018-08-05 02:15:56 +0900

    08/05/18

--- cmd/com.sh
+++ cmd/com.sh
@@ -1,8 +1,9 @@
-#in1=$(date + "%D")
+in1=$(date +%D)
+git commit -m $in1
 
-if [ -z $1 ];then
-    echo "usage"
-else
-git commit -m $1
-fi
+#if [ -z $1 ];then
+    #echo "usage"
+#else
+#git commit -m $1
+#fi
 
---
+++ cmd/cp.sh
@@ -0,0 +1,16 @@
+xp=/mnt/dat/exp
+dir1=$xp/aid/3bs
+dir2=$xp/git/3bs
+
+cp -r \
+$dir1/public/img \
+$dir2/public/img
+
+cp -r \
+$dir1/routes/shop/pal/cnf.json \
+$dir2/routes/shop/pal/cnf.json
+
+cp -r \
+$dir1/routes/shop/aid/son \
+$dir2/routes/shop/aid/son
+

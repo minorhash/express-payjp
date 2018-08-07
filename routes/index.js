d637113f46b9de9f20cea98bd849f032e8e6150c
minorhash <minorhash@gmail.com>
2018-08-07 14:46:17 +0900

    08/07/18

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require('cardb');
+var allmer = db.allMer();
 
 // === get
-var cb = function(req, res, next) {
-  var robj = {
-    title: 'top',
-  };
-  res.render('index', robj);
-};
 
-router.get('/', cb);
+var url=["","news","info","profile","disc","schedule","video","mail","notation"]
+
+for(let i=0;i<url.length;i++){
+router.get("/"+url[i],function(req,res,next) {
+res.render(url[i], {
+title:url[i],
+mer:allmer
+});
+});
+}
+//
 
 module.exports = router;

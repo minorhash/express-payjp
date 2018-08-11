cb3010d2fc20a996213c1119dc87ae1af29f9973
minhash <minorhash@gmail.com>
2018-08-11 23:54:40 +0900

    08/11/18

--- cmd/set.sh
+++ cmd/set.sh
@@ -1,11 +1,8 @@
 #git remote add origin git@github.com:minorhash/3bs.git
-<<<<<<< HEAD
 dir=$(pwd)
 str=${dir:17}
 echo $str
 
 git remote set-url origin git@github.com:minorhash/$str.git
-=======
 git remote set-url origin git@github.com:minorhash/3bs.git
->>>>>>> b4e42674530a2cec2c6936aca8a2c2ca55056558
 

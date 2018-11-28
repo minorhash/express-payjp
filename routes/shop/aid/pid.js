761b3077bae0390e242f814837eec391c5d39504
minorhash <minorhash@gmail.com>
2018-11-28 18:46:42 +0900

    11/28/18

--- routes/shop/aid/pid.js
+++ routes/shop/aid/pid.js
@@ -9,12 +9,11 @@ var snde = require('snd-ema');
 
 // === glob =============================
 var email, dat, pid, str, mai, mnt, usr, sku;
-var mailusr,mailadr;
+var mailusr;
 var inspid, getpid, selpid, strbuy, strite;
-var buy, ite, oite,gpid;
-var mes,sub;
-var sndboo=false;
-var cnf=require("../son/cnf.json");
+var buy, ite, oite,gpid
+
+var cnf=require("../son/aid.json")
 //var sec=cnf.sec;
 var sec=cnf.skl;
 
@@ -32,20 +31,14 @@ else{usr=null;console.log("no usr")}
 }else{console.log("no pss")}
 next()};
 
-var getAdr= function(req, res, next) {
-
-next()};
-
 var putPid = function(req, res, next) {
 //res.redirect("pid")
 
 console.log('=== putPid ===');
 
-var utc = new Date().toJSON().slice(0,10).replace(/-/g,"/")
-var tim=utc.replace(/\//g,"-")
-
+var utc = new Date().toJSON().slice(0,10);
 if (req.body && email) {
-    pid = req.body.id;
+pid = req.body.id;
 
 age
 .get('https://api.paidy.com/payments/'+pid)
@@ -53,67 +46,99 @@ age
 .set("Paidy-Version", "2018-04-10")
 .set("Authorization", "Bearer"+sec)
 .then(res => {
-console.log(email)
-console.log(pid)
-console.log(tim)
-console.log(res.body.amount)
-console.log(res.body.buyer)
-
-oite=res.body.order.items
-console.log(oite)
-
-try{
-adb.insPid(email,pid,res.body.amount,JSON.stringify(res.body.buyer),JSON.stringify(res.body.order.items),tim);
-sndboo=true;
-}catch(err){sndboo=false;
-console.log(err);}
-
-if(oite){
-var i18=require("../../../i18n/shop/ja.json")
-for(var i=0;i<oite.length;i++){
-
-mes=i18.lin1
-+i18.cau1+i18.cau3
-    +i18.lin1
-+usr+"様<br><br>"
-    +i18.cau2+"<br>"
-    +i18.cau3+"<br>"
-    +i18.cau4+"<br>"
-
-+i18.cont+i18.pid+":"+pid+"<br>"
-+i18.title+":"+JSON.stringify(oite[i].title)+"<br>"
-+i18.sku+":"+JSON.stringify(oite[i].id)+"<br>"
-+i18.price+":"+JSON.stringify(oite[i].unit_price)+"<br>"
-+i18.unit+":"+JSON.stringify(oite[i].quantity)+"<br><br>"
-   +i18.pay +i18.aid+"<br>"
-
-+i18.ship1+i18.ship2+i18.ship3
-+i18.ship4+i18.ship5
-+i18.misc+i18.lin1+i18.auto1+i18.auto2+i18.lin1
-+i18.adr1+i18.adr2+i18.adr3
-}
-}
-
+    //console.log(res.body.buyer);
+adb.insPid(email,pid,res.body.amount,
+JSON.stringify(res.body.buyer),
+JSON.stringify(res.body.order.items),
+utc);
 
 })
+
 } else {
+//var    pid = 'pay_Wz8zdysAAF0AirLI'
 console.log("no pid");  }
+next()};
+
+var getPid= function(req, res, next) {
+//gpid=adb.pidPid(pid)
+
+console.log(pid)
+console.log(ite)
+//ite=gpid.ite
+//oite=JSON.parse(ite)
 next()};
 
 var senEma = function(req, res, next) {
 console.log('=== senEma =======================================');
-//email="jinjasaisen@gmail.com"
-    if(sndboo=true){
-sub=i18.buy
-snde.trEma(email,sub,mes);
-    }else{console.log("cant send email");}
+age
+.get('https://api.paidy.com/payments/'+pid)
+.set("Content-Type", "application/json")
+.set("Paidy-Version", "2018-04-10")
+.set("Authorization", "Bearer"+sec)
+.then(res => {
+    //console.log(res.body.buyer);
+//var email="jinjasaisen@gmail.com"
+
+var i18=require("../../../i18n/shop/ja.json")
+var sub=i18.buy
+
+var mes=
+i18.lin1
++i18.cau1
++i18.lin1+"<br>"
++usr+"様<br><br>"
++i18.cau2+"<br><br>"
++i18.cau3
++i18.cau4+"<br>"
+
++i18.cont+"<br>"
++i18.pid+pid+"<br><br>"
+
+var loo="";
+oite=res.body.order.items
+
+for(var i=0;i<oite.length;i++){
+loo+=
+i18.sku+oite[i].id+"<br>"
++i18.title+oite[i].title+"<br>"
++i18.price+(oite[i].unit_price).toLocaleString()+i18.yen+"<br>"
++i18.unit+oite[i].quantity+"<br>"
++i18.lin1
+}
+
+var msum=i18.sub+(res.body.amount-650).toLocaleString()+i18.yen+"<br>"
++i18.cour+650+i18.yen+"<br>"
++i18.sum+(res.body.amount).toLocaleString()+"<br>"
++i18.pay+"paidy"+"<br><br>"
+var ship=
+i18.ship1+i18.ship2+i18.ship3
++i18.ship4+i18.ship5
++i18.misc+i18.lin1+i18.auto1+i18.auto2+i18.lin1
++i18.adr1+i18.adr2+i18.adr3
+
+var fin=mes+loo+msum+ship
+
+
+if(pid){
+snde.trEma(email,sub,fin);
+}else{console.log("no pid")}
+
+
+})
+
+
 next()};
 
 var chk = function(req, res, next) {
-  console.log('=== pid =======================================');
-console.log(mailusr);
+  console.log('=== PID =======================================');
+  console.log(email);
+  console.log(pid);
+  console.log('=== PID =======================================');
 };
 
-router.put('/shop/aid/pid', [getEma, getUsr,putPid,
-chk]);
-module.exports = router;
+var fun=
+[getEma, getUsr,putPid,senEma,
+chk]
+router.put('/shop/aid/pid',fun);
+
+    module.exports = router;

482cbf1651afb30591a544f7d34dedd4ae69a9a4
minorhash <minorhash@gmail.com>
2019-02-02 10:05:15 +0900

    02/02/19

--- routes/shop/aid/tmp.js
+++
@@ -1,8 +0,0 @@
-var    pid = 'pay_Wz8zdysAAF0AirLI'
-var age=require("./age")
-var str=age.get(pid)
-console.log("=== ===")
-//console.log(str)
-var utc = new Date().toJSON().slice(0,10).replace(/-/g,"/")
-console.log(utc)
-
